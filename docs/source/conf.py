0210a6d65e46288162edc1db0d64b7c9866368b1
Yang1030 <117822646+ouyang1030@users.noreply.github.com>
2025-11-05 08:13:19 +0800

    Update Sphinx configuration to use myst_parser

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark'] 
+extensions = ['myst_parser'] 
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -32,3 +32,4 @@ import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
 # html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
